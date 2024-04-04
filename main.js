a5a5fdc039eae964a919bb368aab8bcd7386b893
AmriteshRaj123 <pathakamritesh6@gamil.com>
2024-04-04 23:01:08 +0530

    first update

---
+++ main.js
@@ -0,0 +1,16 @@
+function red(){
+    document.getElementsByClassName('red')[0].style.backgroundColor = "red";
+    document.getElementsByClassName('yellow')[0].style.backgroundColor = "rgb(233, 233, 195)";
+    document.getElementsByClassName('green')[0].style.backgroundColor = "rgb(198, 237, 198)";
+}
+function yellow(){
+    document.getElementsByClassName('yellow')[0].style.backgroundColor = "yellow";
+    document.getElementsByClassName('green')[0].style.backgroundColor = "rgb(198, 237, 198)";
+    document.getElementsByClassName('red')[0].style.backgroundColor = "rgb(240, 193, 193)";
+}
+function green(){
+    document.getElementsByClassName('green')[0].style.backgroundColor = "green";
+    document.getElementsByClassName('red')[0].style.backgroundColor = "rgb(240, 193, 193)";
+    document.getElementsByClassName('yellow')[0].style.backgroundColor = "rgb(233, 233, 195)";
+}
+
